8e6dbf65872d4bcded63e3d47d3591b70a272216
unknown <you@example.com>
2023-12-12 22:07:40 +0100

    optimised

--- rust/aoc2023/src/days/day12.rs
+++ rust/aoc2023/src/days/day12.rs
@@ -2,6 +2,7 @@ use std::collections::HashMap;
 
 use super::super::dayx::*;
 use super::super::ulitity::*;
+use std::time::Instant;
 
 type State = (usize, usize);
 
@@ -10,45 +11,57 @@ type State = (usize, usize);
 //  remove group from string
 //  pass remaining string and group
 
-fn options(s: &String, sstart: usize, groups: &Vec<usize>, gstart: usize, cache: &mut HashMap<State, i64>) -> i64 {
-    if groups.len() <= gstart {
-        if !s.chars().skip(sstart).any(|c| c == '#') {
-            return 1;
-        }
-        return 0;
-    } else if !s.chars().skip(sstart).any(|c| c == '#' || c == '?') {
-        return 0;
+fn options(
+    s: &String,
+    sstart: usize,
+    groups: &Vec<usize>,
+    gstart: usize,
+    cache: &mut HashMap<State, i64>,
+) -> i64 {
+    let state = (sstart, gstart);
+    if cache.contains_key(&state) {
+        return *cache.get(&state).unwrap();
     }
+
+    // no more remaining string left
+    if s.len() <= sstart {
+        // valid if no more groups left either
+        return if groups.len() <= gstart { 1 } else { 0 };
+    }
+    // no more groups left
+    if groups.len() <= gstart {
+        // valid if no more '#' left in the remaining string
+        return if s.chars().skip(sstart).any(|e| e == '#') { 0 } else { 1 };
+    }
+    // for n groups we need at least n * 2 - 1 chars left since every group must have a '.' or '?' seperator
+    if (groups.len() - gstart) * 2 - 1 > s.len() - sstart { return 0; }
 
     let mut ans = 0;
     let group = groups[gstart];
-    for i in sstart..s.len() {
-        let c = s.chars().nth(i).unwrap();
-        if c == '#' || c == '?' {
-            let cond1 =
-                i + group <= s.len() && !(i..i + group).any(|i| s.chars().nth(i).unwrap() == '.');
-            let cond2 = i + group >= s.len() || s.chars().nth(i + group).unwrap() != '#';
-            if cond1 && cond2 {
-                let _sstart: usize = i + group + 1;
-                let _gstart: usize = gstart + 1;
+    let c = s.chars().nth(sstart).unwrap();
+    if c == '#' || c == '?' {
+        // current group must fit in the string we have left
+        // no '.' in s covered by the group (lookahead)
+        // the char in s after the group must be '.' (or '?')
 
-                // let sl = s.substring(0, i);
-                // let sr = s.substring(i + group, s.len() - i + group);
-                // println!("{:?}", sl + &"x".repeat(group) + &sr);
+        let cond1 =
+            sstart + group <= s.len() && !(sstart..sstart + group).any(|i| s.chars().nth(i).unwrap() == '.');
+        let cond2 = sstart + group >= s.len() || s.chars().nth(sstart + group).unwrap() != '#';
+        if cond1 && cond2 {
+            let _sstart: usize = sstart + group + 1;
+            let _gstart: usize = gstart + 1;
 
-                let state = (_sstart, _gstart);
-                if !cache.contains_key(&state) {
-                    let ans = options(&s, _sstart, &groups, _gstart, cache);
-                    cache.insert(state, ans);
-                }
-                ans += cache.get(&state).unwrap();
-            }
-            if c == '#' {
-                break;
-            }
+            ans += options(&s, _sstart, &groups, _gstart, cache);
         }
     }
+    if c == '.' || c == '?' {
+        let _sstart: usize = sstart + 1;
+        let _gstart: usize = gstart;
 
+        ans += options(&s, _sstart, &groups, _gstart, cache);
+    }
+
+    cache.insert(state, ans);
     return ans;
 }
 
@@ -58,26 +71,25 @@ pub fn day() {
     let lines = read_lines(&input_path(day));
 
     {
+        let now = Instant::now();
         let mut col = vec![];
         for line in lines.iter() {
             let split = string_split(&line, " ");
             let groups: Vec<usize> = string_to_ints(&line).iter().map(|e| *e as usize).collect();
             col.push((split[0].clone(), groups));
         }
+        println!("Lines in {:?}", now.elapsed());
 
-        let parts = vec![
-            (1, 1),
-            (2, 5)
-        ];
+        let parts = vec![(1, 1), (2, 5)];
         for &(part, repeating) in parts.iter() {
-            use std::time::Instant;
             let now = Instant::now();
             let mut ans = 0i64;
             for (s, group) in col.iter() {
-                let s = (0..repeating).map(|_| s.to_string()).collect::<Vec<String>>().join("?");
-                let group = (0..repeating)
-                    .flat_map(|_| group.iter().copied())
-                    .collect();
+                let s = (0..repeating)
+                    .map(|_| s.to_string())
+                    .collect::<Vec<String>>()
+                    .join("?");
+                let group = (0..repeating).flat_map(|_| group.iter().copied()).collect();
                 let _ans = options(&s, 0, &group, 0, &mut HashMap::new());
                 ans += _ans;
             }
